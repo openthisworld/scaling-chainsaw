b7809317c596ff3ef524477586601edfe25480dd
Vlad Huivyk <90330519+openthisworld@users.noreply.github.com>
2024-04-24 00:11:34 +0300

    change: eks code

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,33 +1,91 @@
 provider "aws" {
-  region = "us-east-1"
+  region = local.region
 }
 
-module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = "my-eks-cluster"
-  cluster_version = "1.21"
-  subnets         = [aws_subnet.my_subnet.id]
-  vpc_id          = aws_vpc.my_vpc.id
+locals {
+  name   = "ascode-cluster"
+  region = "us-east-1"
 
-  node_groups = {
-    example = {
-      desired_capacity = 1
-      max_capacity     = 2
-      min_capacity     = 1
+  vpc_cidr = "10.123.0.0/16"
+  azs      = ["us-east-1a", "us-east-1b"]
 
-      instance_type = "t3.medium"
-    }
+  public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
+  private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
+  intra_subnets   = ["10.123.5.0/24", "10.123.6.0/24"]
+
+  tags = {
+    Example = local.name
   }
 }
 
-output "cluster_id" {
-  value = module.eks.cluster_id
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 4.0"
+
+  name = local.name
+  cidr = local.vpc_cidr
+
+  azs             = local.azs
+  private_subnets = local.private_subnets
+  public_subnets  = local.public_subnets
+  intra_subnets   = local.intra_subnets
+
+  enable_nat_gateway = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
 }
 
-output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
-}
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.15.1"
 
-output "kubeconfig" {
-  value = module.eks.kubeconfig
+  cluster_name                   = local.name
+  cluster_endpoint_public_access = true
+
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
+
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
+  control_plane_subnet_ids = module.vpc.intra_subnets
+
+  # EKS Managed Node Group(s)
+  eks_managed_node_group_defaults = {
+    ami_type       = "AL2_x86_64"
+    instance_types = ["m5.large"]
+
+    attach_cluster_primary_security_group = true
+  }
+
+  eks_managed_node_groups = {
+    ascode-cluster-wg = {
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+
+      instance_types = ["t3.large"]
+      capacity_type  = "SPOT"
+
+      tags = {
+        ExtraTag = "openthisworld"
+      }
+    }
+  }
+
+  tags = local.tags
 }
